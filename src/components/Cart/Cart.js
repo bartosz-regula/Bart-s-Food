8eb90a4da689cca4f9283e95a84c70aedc695796
Bartosz Reguła <barreg@o2.pl>
2023-04-07 22:13:58 +0200

    Dummy data added to cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,51 @@
 import React from 'react'
 import classes from '../Cart/Cart.module.css'
 
+const DUMMY_CART = [
+	{
+		id: '1',
+		name: 'Burger Extra Large',
+		price: '£12.99'
+
+	},
+	{
+		id: '2',
+		name: 'Kebab Doner',
+		price: '£15.99'
+
+	},
+	{
+		id: '3',
+		name: 'Barbecue Burger',
+		price: '£13.99'
+	}
+];
+
 
 const Cart = () => {
+
+	const cartList = DUMMY_CART.map(item => (
+		<div className={classes.cart_items}>
+			<span className={classes.item_name}>{item.name}</span>
+			<form className={classes.form}>
+				<div>
+					<label>x</label>
+					<input
+						type='number'
+						min='1'
+						max='12'
+						step='1'
+						defaultValue='1' />
+				</div>
+				<button>
+					<ion-icon name="trash-outline"></ion-icon>
+				</button>
+				<span>{item.price}</span>
+			</form>
+		</div>
+	));
+
+
 	return (
 		<div className={classes.container}>
 			<div className={classes.content}>
@@ -13,62 +56,10 @@ const Cart = () => {
 					</h2>
 					<p> 10£ missing to free delivery</p>
 				</div>
-				<div>
-					<div className={classes.cart_items}>
-						<span className={classes.item_name}>Burger</span>
-						<form className={classes.form}>
-							<div>
-								<label>x</label>
-								<input
-									type='number'
-									min='1'
-									max='12'
-									step='1'
-									defaultValue='1' />
-							</div>
-							<button>							<ion-icon name="trash-outline"></ion-icon>
-							</button>
-							<span>£12.99</span>
-						</form>
-					</div>
-					<div className={classes.cart_items}>
-						<span className={classes.item_name}>Lamb Doner Kebab Extra </span>
-						<form className={classes.form}>
-							<div>
-								<label>x</label>
-								<input
-									type='number'
-									min='1'
-									max='12'
-									step='1'
-									defaultValue='1' />
-							</div>
-							<button>							<ion-icon name="trash-outline"></ion-icon>
-							</button>
-							<span>£12.99</span>
-						</form>
-					</div>
-					<div className={classes.cart_items}>
-						<span className={classes.item_name}>Pepperoni Pizza</span>
-						<form className={classes.form}>
-							<div>
-								<label>x</label>
-								<input
-									type='number'
-									min='1'
-									max='12'
-									step='1'
-									defaultValue='1' />
-							</div>
-							<button>							<ion-icon name="trash-outline"></ion-icon>
-							</button>
-							<span>£12.99</span>
-						</form>
-					</div>
 
-				</div>
+				{cartList}
+
 				<div className={classes.summary} >
-
 					<div className={classes.subtotal}>
 						<span>Subtotal</span>
 						<span>£37.50</span>
